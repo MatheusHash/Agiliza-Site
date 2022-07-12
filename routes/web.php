4f1131fbc69e0409254ac75a491d2e78b6b70bfc
MatheusHASH <matheusferreirapimenta1@gmail.com>
2022-07-12 15:39:32 -0300

    criando rotas para delete e update

--- routes/web.php
+++ routes/web.php
@@ -30,28 +30,30 @@ Route::middleware('auth')->group(function (){ //Middleware para autenticar o usu
             // Rotas para formulario e adicionar um Imovel no BD
             Route::get('/adicionar', [App\Http\Controllers\admin\ImovelController::class, 'page'])->name('imoveis.store.form');
             Route::post('/adicionar',[App\Http\Controllers\admin\ImovelController::class, 'store'])->name('imoveis.store');
-            
+
             // Rotaa para listar todos os imoveis
             Route::get('/listaDeImoveis', [App\Http\Controllers\admin\ImovelController::class,'show'])->name('imoveis.show');
-            
+
             // Rota para um mostrar imovel pelo ID
             Route::get('/{id}', [App\Http\Controllers\admin\ImovelController::class,'showById'])->name('showById.index');
 
             // Rota para mostrar o formulario de update do Imovel
             Route::get('/{id}/editar',[App\Http\Controllers\admin\ImovelController::class, 'edit'])->name('imoveis.edit');
             Route::put('/{id}/update',[App\Http\Controllers\admin\ImovelController::class, 'update'])->where('id', '[0-9]+')->name('imoveis.update');
-       
+            
+            // Rota para deletar um ímovel //=> No controller também será erxcluído as imagens da galeria
+            Route::delete('/{id}/delete',[App\Http\Controllers\admin\ImovelController::class, 'destroy'])->where('id', '[0-9]+')->name('imoveis.destroy');
+            
+            // Rota para alterar a visibilidade de um ímovel
+            Route::put('/{id}/visibilidade',[App\Http\Controllers\admin\ImovelController::class, 'visibility'])->where('id', '[0-9]+')->name('imoveis.visibility');
+            
         });
-
-
 
         Route::prefix('cidades')->group(function(){
             // Rotas para listar e Adicionar cidades no BD
             Route::get('/', [App\Http\Controllers\admin\CidadeController::class, 'index'])->name('cidades.index');
             Route::post('/adicionar', [App\Http\Controllers\admin\CidadeController::class, 'store'])->name('cidades.store');
-
         });
-
     });
 });
 
